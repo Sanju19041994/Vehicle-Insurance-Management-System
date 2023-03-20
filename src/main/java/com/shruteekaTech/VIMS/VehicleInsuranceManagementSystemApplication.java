ddf34493fc4c1fb30adc6fa98c0c18b9ad58a407
Sanju19041994 <Sanjuchakole.sanju@gmail.com>
2023-03-20 23:14:52 +0530

    VIMS 1 : Create Spring Boot Project : Vehicle Insurance Management System

---
+++ src/main/java/com/shruteekaTech/VIMS/VehicleInsuranceManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.shruteekaTech.VIMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicleInsuranceManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehicleInsuranceManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shruteekaTech/VIMS/VehicleInsuranceManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shruteekaTech.VIMS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleInsuranceManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
