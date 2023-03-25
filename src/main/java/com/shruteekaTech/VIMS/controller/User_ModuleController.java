abc9e90763faacc73d14733281daa64479325009
Sanju19041994 <Sanjuchakole.sanju@gmail.com>
2023-03-25 19:21:44 +0530

    VIMS 6 : Implement Looger in DAO & Controller Layer

--- src/main/java/com/shruteekaTech/VIMS/controller/User_ModuleController.java
+++ src/main/java/com/shruteekaTech/VIMS/controller/User_ModuleController.java
@@ -4,6 +4,8 @@ import com.shruteekaTech.VIMS.dto.UserDtoModule;
 import com.shruteekaTech.VIMS.service.User_ModuleService;
 import com.shruteekaTech.VIMS.utils.ApiResponse;
 import com.shruteekaTech.VIMS.utils.UserLogin;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,14 +17,17 @@ import java.util.List;
 @RequestMapping("/user")
 public class User_ModuleController {
 
+    Logger logger = LoggerFactory.getLogger(User_ModuleController.class);
     @Autowired
     private User_ModuleService userModuleService;
 
 
     @PostMapping("/save")
     ResponseEntity<ApiResponse> saveNewUser(@RequestBody UserDtoModule userDtoModule){
+        logger.info("Started : saveNewUser() started from User_ModuleController");
         boolean saveUser = userModuleService.saveUser(userDtoModule);
         if(saveUser){
+            logger.info("Completed : saveNewUser() completed from User_ModuleController");
             return new ResponseEntity<>(new ApiResponse("User Saved successfully",true), HttpStatus.CREATED);
         }else {
             throw new RuntimeException("User Not Saved To DB");
@@ -31,8 +36,10 @@ public class User_ModuleController {
 
     @PutMapping("/updateID/{userId}")
     ResponseEntity<ApiResponse> updateUsers(@RequestBody UserDtoModule userDtoModule, @PathVariable Integer userId){
+        logger.info("Started : updateUsers() started from User_ModuleController");
         boolean updateUser = userModuleService.updateUser(userId, userDtoModule);
         if(updateUser){
+            logger.info("Completed : updateUsers() completed from User_ModuleController");
             return new ResponseEntity<>(new ApiResponse("User Updated successfully",true), HttpStatus.OK);
         }else {
             throw new RuntimeException("User Not Updated");
@@ -41,38 +48,50 @@ public class User_ModuleController {
 
     @GetMapping("/userId/{userId}")
     ResponseEntity<UserDtoModule> getSingleUserById(@PathVariable Integer userId){
+        logger.info("Started : getSingleUserById() started from User_ModuleController");
         UserDtoModule userById = userModuleService.getUserById(userId);
+        logger.info("Completed : getSingleUserById() completed from User_ModuleController");
         return new ResponseEntity<>(userById,HttpStatus.FOUND);
     }
 
     @GetMapping("/email/{email}")
     ResponseEntity<UserDtoModule> getSingleUserByEmail(@PathVariable String email){
+        logger.info("Started : getSingleUserByEmail() started from User_ModuleController");
         UserDtoModule userByEmail = userModuleService.getUserByEmail(email);
+        logger.info("Completed : getSingleUserByEmail() completed from User_ModuleController");
         return new ResponseEntity<>(userByEmail,HttpStatus.FOUND);
     }
 
     @GetMapping("/name/{name}")
     ResponseEntity<List<UserDtoModule>> getUsersByName(@PathVariable String name){
+        logger.info("Started : getUsersByName() started from User_ModuleController");
         List<UserDtoModule> userByName = userModuleService.getUserByName(name);
+        logger.info("Completed : getUsersByName() completed from User_ModuleController");
         return new ResponseEntity<>(userByName,HttpStatus.FOUND);
     }
 
     @GetMapping("/allUsers")
     ResponseEntity<List<UserDtoModule>> getUsersList(){
+        logger.info("Started : getUsersList() started from User_ModuleController");
         List<UserDtoModule> allUsers = userModuleService.getAllUsers();
+        logger.info("Completed : getUsersList() completed from User_ModuleController");
         return new ResponseEntity<>(allUsers,HttpStatus.FOUND);
     }
 
     @DeleteMapping("/{userId}")
     ResponseEntity<ApiResponse> deleteUser(@PathVariable Integer userId){
+        logger.info("Started : deleteUser() started from User_ModuleController");
         userModuleService.deleteUser(userId);
+        logger.info("Completed : deleteUser() completed from User_ModuleController");
         return new ResponseEntity<>(new ApiResponse("User Deleted Successfully",true),HttpStatus.OK);
     }
 
     @PostMapping("/login")
     public ResponseEntity<ApiResponse> login(@RequestBody UserLogin userLogin){
+        logger.info("Started : login() started from User_ModuleController");
         boolean loginCheck = userModuleService.loginCheck(userLogin);
         if(loginCheck){
+            logger.info("Completed : login() completed from User_ModuleController");
             return new ResponseEntity<>(new ApiResponse("Login successfully",true),HttpStatus.FOUND);
         }else {
             return new ResponseEntity<>(new ApiResponse("Login fail..Please check your email or password",false),HttpStatus.NOT_FOUND);
--- src/main/java/com/shruteekaTech/VIMS/service/Impl/User_ModuleServiceImpl.java
+++ src/main/java/com/shruteekaTech/VIMS/service/Impl/User_ModuleServiceImpl.java
@@ -7,6 +7,8 @@ import com.shruteekaTech.VIMS.repository.User_ModuleRepo;
 import com.shruteekaTech.VIMS.service.User_ModuleService;
 import com.shruteekaTech.VIMS.utils.UserLogin;
 import org.modelmapper.ModelMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,6 +17,8 @@ import java.util.stream.Collectors;
 
 @Service
 public class User_ModuleServiceImpl implements User_ModuleService {
+
+    Logger logger = LoggerFactory.getLogger(User_ModuleServiceImpl.class);
 
     @Autowired
     private User_ModuleRepo userModuleRepo;
@@ -26,10 +30,12 @@ public class User_ModuleServiceImpl implements User_ModuleService {
 
     @Override
     public boolean saveUser(UserDtoModule userDtoModule) {
+        logger.info("Started : saveUser() started from User_ModuleServiceImpl");
         User_Module user = modelMapper.map(userDtoModule, User_Module.class);
       //  user.setPassword(passwordEncoder.encode(user.getPassword()));
         User_Module savedUser = userModuleRepo.save(user);
         if(savedUser != null){
+            logger.info("Completed : saveUser() completed from User_ModuleServiceImpl");
             return true;
         }
         return false;
@@ -37,6 +43,7 @@ public class User_ModuleServiceImpl implements User_ModuleService {
 
     @Override
     public boolean updateUser(Integer userId, UserDtoModule userDtoModule) {
+        logger.info("Started : updateUser() started from User_ModuleServiceImpl");
         User_Module user = userModuleRepo.findById(userId).orElseThrow(() -> new ResourceNotFoundException("User","userId"));
 
         user.setName(userDtoModule.getName());
@@ -48,6 +55,7 @@ public class User_ModuleServiceImpl implements User_ModuleService {
 
         User_Module updatedUser = userModuleRepo.save(user);
         if(updatedUser != null){
+            logger.info("Completed : updateUser() completed from User_ModuleServiceImpl");
             return true;
         }
         return false;
@@ -55,48 +63,60 @@ public class User_ModuleServiceImpl implements User_ModuleService {
 
     @Override
     public UserDtoModule getUserById(Integer userId) {
+        logger.info("Started : getUserById() started from User_ModuleServiceImpl");
         User_Module user = userModuleRepo.findById(userId).orElseThrow(() -> new ResourceNotFoundException("User","userId"));
         UserDtoModule dtoModule = modelMapper.map(user, UserDtoModule.class);
+        logger.info("Completed : getUserById() completed from User_ModuleServiceImpl");
         return dtoModule;
     }
 
     @Override
     public UserDtoModule getUserByEmail(String email) {
+        logger.info("Started : getUserByEmail() started from User_ModuleServiceImpl");
         User_Module user = userModuleRepo.findByEmail(email).orElseThrow(() -> new ResourceNotFoundException("User","email"));
         UserDtoModule dtoModule = modelMapper.map(user, UserDtoModule.class);
+        logger.info("Completed : getUserByEmail() completed from User_ModuleServiceImpl");
         return dtoModule;
     }
 
     @Override
     public List<UserDtoModule> getUserByName(String name) {
+        logger.info("Started : getUserByName() started from User_ModuleServiceImpl");
         List<User_Module> userList = userModuleRepo.findByName(name).orElseThrow(() -> new ResourceNotFoundException("User","name"));
         List<UserDtoModule> list = userList.stream().map((users) ->
                              this.modelMapper.map(users, UserDtoModule.class)).collect(Collectors.toList());
+        logger.info("Completed : getUserByName() completed from User_ModuleServiceImpl");
         return list;
     }
 
     @Override
     public List<UserDtoModule> getAllUsers() {
+        logger.info("Started : getAllUsers() started from User_ModuleServiceImpl");
         List<User_Module> all = userModuleRepo.findAll();
         List<UserDtoModule> userList = all.stream().map((users) ->
                            this.modelMapper.map(users, UserDtoModule.class)).collect(Collectors.toList());
+        logger.info("Completed : getAllUsers() completed from User_ModuleServiceImpl");
         return userList;
     }
 
     @Override
     public void deleteUser(Integer userId) {
+        logger.info("Started : deleteUser() started from User_ModuleServiceImpl");
         User_Module user = userModuleRepo.findById(userId).orElseThrow(()-> new ResourceNotFoundException("User","userId"));
         userModuleRepo.delete(user);
+        logger.info("Completed : deleteUser() completed from User_ModuleServiceImpl");
     }
 
     @Override
     public boolean loginCheck(UserLogin userLogin) {
+        logger.info("Started : loginCheck() started from User_ModuleServiceImpl");
         String email = userLogin.getEmail();
         String password = userLogin.getPassword();
 
         User_Module user = userModuleRepo.findByEmailAndPassword(email, password).orElseThrow(() -> new ResourceNotFoundException("User", "email or password"));
 
         if(user.getEmail().equals(email) && user.getPassword().equals(password)){
+            logger.info("Completed : loginCheck() completed from User_ModuleServiceImpl");
             return true;
         }
         return false;
