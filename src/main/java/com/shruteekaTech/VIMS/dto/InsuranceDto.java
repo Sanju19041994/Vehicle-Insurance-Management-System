af1408c4e08cf457f3ef8c38f1b73b6e72dde259
Sanju19041994 <Sanjuchakole.sanju@gmail.com>
2023-03-27 20:04:45 +0530

    VIMS 16 : Create InsuranceDto class

--- src/main/java/com/shruteekaTech/VIMS/dto/InsuranceDto.java
+++ src/main/java/com/shruteekaTech/VIMS/dto/InsuranceDto.java
@@ -1,9 +1,24 @@
 package com.shruteekaTech.VIMS.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.time.LocalDate;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class InsuranceDto {
 
+    private Integer id;
 
+    private String provider;
+
+    private String insuranceName;
+
+    private LocalDate insuranceValidity;
+
+    private UserDtoModule userModule;
 
 }
