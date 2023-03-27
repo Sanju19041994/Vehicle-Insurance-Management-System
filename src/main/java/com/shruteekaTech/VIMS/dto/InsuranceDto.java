7ad893eb7364fb0d9747fa79daff7452403fdc1e
Sanju19041994 <Sanjuchakole.sanju@gmail.com>
2023-03-27 20:12:20 +0530

    VIMS 17 : Add validation annotation in InsuranceDto feilds

--- src/main/java/com/shruteekaTech/VIMS/dto/InsuranceDto.java
+++ src/main/java/com/shruteekaTech/VIMS/dto/InsuranceDto.java
@@ -1,5 +1,8 @@
 package com.shruteekaTech.VIMS.dto;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,10 +16,13 @@ public class InsuranceDto {
 
     private Integer id;
 
+    @NotNull @NotEmpty @Size(min = 2, max = 50)
     private String provider;
 
+    @NotNull @NotEmpty @Size(min = 2, max = 100)
     private String insuranceName;
 
+    @NotNull @NotEmpty
     private LocalDate insuranceValidity;
 
     private UserDtoModule userModule;
